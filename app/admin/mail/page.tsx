c240f84a19d5697e2d9167261bac6f371612ded8
yaviinteriorwebsite01-stack <yaviinteriorwebsite01@gmail.com>
2026-09-04 12:10:14 +0530

    Update admin mail

--- app/admin/mail/page.tsx
+++ app/admin/mail/page.tsx
@@ -222,7 +222,7 @@ export default function AdminMailPage() {
                           ) : (
                             <>
                               <RotateCw className="w-3.5 h-3.5" />
-                              <span>Resend Mail</span>
+                              <span></span>
                             </>
                           )}
                         </button>
